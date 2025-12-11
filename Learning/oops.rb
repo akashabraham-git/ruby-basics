8424ceb031f51984a5cecd7b7913643ae8dab3a3
Akash Abraham <akash.abraham@rently.com>
2025-12-11 09:31:27 +0530

    include exclude

---
+++ Learning/oops.rb
@@ -0,0 +1,51 @@
+#mixin
+module Walkable
+  def walk
+    puts "#{@name} is walking"
+  end
+end
+#use include to import methods as instance methods and extend to import methods as class methods
+
+class Animal
+  include Walkable 
+  def initialize(name)
+    @name = name          # instance variable; encapsulated
+    @secret = "hidden"    # encapsulation: no getter, private access
+  end
+
+  def speak
+    puts "sound"
+  end
+
+  private
+  def secret_info
+    puts @secret
+  end
+end
+
+#inheritance
+class Dog < Animal
+  def initialize(name, breed)
+    #super
+    super(name)         
+    @breed = breed
+  end
+
+#polymorphism
+  def speak
+    puts "#{@name} the #{@breed} says: Woof!"
+  end
+
+  def info
+    puts "Breed: #{@breed}"
+  end
+end
+
+a = Animal.new("John")
+a.speak
+
+d = Dog.new("Buddy", "Golden Retriever")
+d.speak
+d.info
+d.walk     
+
